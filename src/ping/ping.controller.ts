e39c0e662598a5990bb852b0e101d463f52703d4
Phạm Đức Toàn <phamductoan220704@gmail.com>
2025-07-17 21:54:11 +0700

    basic

--- src/ping/ping.controller.ts
+++ src/ping/ping.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class PingController {
   @Get()
   getPing() {
-    return { message: 'pong' };
+    return { message: process.env.PING };
   }
 }
